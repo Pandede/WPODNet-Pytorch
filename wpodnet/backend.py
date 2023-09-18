b129c243ba96f4c8436e5ebe4793f5cd0c31378d
Pandede <steven9609@gmail.com>
2023-09-18 05:55:31 +0000

    use `item()` to get single element in array

--- wpodnet/backend.py
+++ wpodnet/backend.py
@@ -121,5 +121,5 @@ class Predictor:
         return Prediction(
             image=image,
             bounds=bounds.astype(np.int32),
-            confidence=max_prob
+            confidence=max_prob.item()
         )
